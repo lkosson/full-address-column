22fa9d13a51a645866a6b1335095d40e86894c31
Manuel Ströbele <info@stroebele-it.de>
2024-07-21 15:13:47 +0200

    Version 1.2.0. Added feature #18: new columns "sender domain" and "recipients domains", code refactoring

--- src/background.js
+++ src/background.js
@@ -1,2 +1,4 @@
 browser.customColumns.add("senderAddressColumn", "Sender (@)", "sender");
-browser.customColumns.add("recipientAddressColumn", "Recipient (@)", "recipient");
+browser.customColumns.add("senderDomainColumn", "Sender Domain (@)", "sender_domain");
+browser.customColumns.add("recipientsAddressesColumn", "Recipients (@)", "recipients");
+browser.customColumns.add("recipientsDomainsColumn", "Recipients Domains (@)", "recipients_domains");
