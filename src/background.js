3b0d2f7b768b68e61da14fe433614738cffde745
derVedro <derVedro@users.noreply.github.com>
2025-05-09 18:00:19 +0200

    add Sender Reverse Domain column

--- src/background.js
+++ src/background.js
@@ -1,4 +1,5 @@
 browser.customColumns.add("senderAddressColumn", "Sender (@)", "sender");
 browser.customColumns.add("senderDomainColumn", "Sender Domain (@)", "sender_domain");
+browser.customColumns.add("senderReverseDomainColumn", "Sender Reverse Domain (@)", "sender_reverse_domain");
 browser.customColumns.add("recipientsAddressesColumn", "Recipients (@)", "recipients");
 browser.customColumns.add("recipientsDomainsColumn", "Recipients Domains (@)", "recipients_domains");
